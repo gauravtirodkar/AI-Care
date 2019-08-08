2017ad11550bf451715a75cb7d81ad6971403bd7
Sujitkumar Singh <2017.sujitkumar.singh@ves.ac.in>
2019-08-08 16:44:45 +0530

    updates

--- medical.php
+++ medical.php
@@ -22,7 +22,7 @@
 	<!-- Compiled and minified CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-	<title>वृद्ध सेवा</title>
+	<title>VridhhaSeva</title>
 </head>
 
 <body>
@@ -49,23 +49,29 @@
    ?>
 	<!-- NAVIGATION -->
 	<nav class="blue darken-2">
-		<div class="container">
-			<div class="nav-wrapper">
-				<a href="index.html" class="brand-logo">Vishandas Mehta</a>
-				<a href="#" data-activates="side-nav" class="button-collapse show-on-large right">
-					<i class="material-icons">menu</i>
-				</a>
-				<ul class="right hide-on-med-and-down">
-					<li class="active">
-						<a href="index.html">Dashboard</a>
-					</li>
-					<li>
-						<a href="#map">Emergency Services</a>
-					</li>
-					<li>
-						<a href="#medic">Medicines</a>
-					</li>
-				</ul>
+      <div class="container">
+        <div class="nav-wrapper">
+          <a class="navbar-brand" href="index.html" style="font-size: 25px;"><img src="./images/logo.png" style ="height: 70px; padding:8px; color: transparent;" alt=""> </a>
+          <a href="#" data-activates="side-nav" class="button-collapse show-on-large right">
+		  <i class="material-icons">menu</i>
+          </a>
+          <ul class="right hide-on-med-and-down">
+            <li class="active">
+              <a href="index.html">Home</a>
+			</li>
+			<li>
+              <a href="#map">Emergency Services</a>
+            </li>
+            <li>
+              <a href="#reminder">Reminders</a>
+            </li>
+            <li>
+              <a href="#medic">Prescriptions</a>
+            </li>
+          </ul>
+          </div>
+          </div>
+          </nav>
 				<!-- Side nav -->
 				<ul id="side-nav" class="side-nav">
 					<li>
@@ -125,7 +131,13 @@
 
 	<!-- Notification -->
 	<br>
+
+	<section id="reminder">
 	<div class="container">
+  		<div class="jumbotron" style="padding:3px 50px 3px 50px;">
+		  <h4 style="text-align:center;">Reminders</h4>
+		  </div>
+		
 		<div class="alert alert-success alert-dismissible">
 			<button type="button" class="close" data-dismiss="alert">&times;</button>
 			<strong>Success!</strong> You have successfully completed your morning walk of 30 mins!!
@@ -146,108 +158,130 @@
 			<button type="button" class="close" data-dismiss="alert">&times;</button>
 			<strong>New Task!</strong> Drink 3 litres of water and sleep for 8 hours.
 		</div>
+		</div>
+		<br>
 	</div>
+
+	
+	</section>
 	<!-- SECTION: STATS -->
 	<section class="section section-stats center">
+		<div class="container">
+  		<div class="jumbotron" style="padding:3px 50px 3px 50px;">
+			  <h4 class="">Dashboard</h4>
+		  </div>
+		  </div>
 		<div class="row">
+			<a href="blood-pressure.html" style="text-decoration:none;">
 			<div class="col s12 m6 l3">
 				<div class="card-panel blue lighten-1 white-text center">
 					<i class="material-icons medium">insert_emoticon</i>
 					<h5>Blood Pressure(in bpm)</h5>
 					<h3>162/65</h3>
 					<div class="progress grey lighten-1">
-						<div class="determinate white" style="width: 40%;"></div>
+						<div class="determinate white" style="width: 70%;"></div>
 					</div>
 				</div>
+</a>
 			</div>
+			<a href="temperature.html" style="text-decoration:none;hover:none">
 			<div class="col s12 m6 l3">
 				<div class="card-panel center">
 					<i class="fa fa-4x fa-thermometer-three-quarters"></i>
 					<h5>Body Temperature(in F)</h5>
 					<h3 class="count">99</h3>
 					<div class="progress grey lighten-1">
-						<div class="determinate blue lighten-1" style="width: 20%;"></div>
+						<div class="determinate blue lighten-1" style="width: 75%;"></div>
 					</div>
 				</div>
+</a>
 			</div>
 			<div class="col s12 m6 l3">
+			<a href="heart-rate.html" style="text-decoration:none;hover:none">
 				<div class="card-panel blue lighten-1 white-text center">
 					<i class="fa fa-4x fa-heartbeat"></i>
 					<h5>Pulse Rate</h5>
 					<h3 class="count">75</h3>
 					<div class="progress grey lighten-1">
-						<div class="determinate white" style="width: 58%;"></div>
+						<div class="determinate white" style="width: 78%;"></div>
 					</div>
 				</div>
+</a>
 			</div>
 			<div class="col s12 m6 l3">
+			<a href="sleep.html" style="text-decoration:none;hover:none">
 				<div class="card-panel center">
 					<i class="fa fa-4x fa-bed"></i>
 					<h5>Average Sleep(in hrs)</h5>
 					<h3 class="count">7</h3>
 					<div class="progress grey lighten-1">
-						<div class="determinate blue lighten-1" style="width: 12%;"></div>
+						<div class="determinate blue lighten-1" style="width: 83%;"></div>
 					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col s12 m6 l3">
-
-			</div>
-			<div class="col s12 m6 l3">
-				<div class="card-panel center">
-					<i class="fa fa-4x fa-thermometer-three-quarters"></i>
-					<h5>Body Temperature(in F)</h5>
-					<h3 class="count">99</h3>
-					<div class="progress grey lighten-1">
-						<div class="determinate blue lighten-1" style="width: 20%;"></div>
-					</div>
-				</div>
-			</div>
-			<a onClick="document.location.href='DiseasePrediction/symp1.php'" class="col s12 m6 l3">
-				<div class="card-panel blue lighten-1 white-text center"><br><br>
-					<i class="fa fa-4x fa-paper-plane"></i>
-					<h5>Send Report</h5>
-					<br>
-					<br><br>
 				</div>
 </a>
-			<div class="col s12 m6 l3">
-
 			</div>
 		</div>
-	</section>
+<br><br>
+</section>
+<section id="contact" style="background-color:">
+<div class="container">
+  		<div class="jumbotron" style="padding:3px 50px 3px 50px;">
+		  <h4 style="text-align:center;">Reach a Doctor</h4>
+		  </div>
+		  
+	
+				<div class="card-panel center blue lighten-4">
+					<div class="row">
+						<div class="col s6 m4 l4">
+							<img style="height:300px;width:100%;" src="./images/l.jpg" alt="">
+						</div>
+						<div class="col s12 m4 l4">
+						<br><br>
+						<h3>Feeling Unwell?</h3>
+						<br>
+							<a onClick="document.location.href='DiseasePrediction/symp1.php'"><i class="fa fa-4x fa-paper-plane"></i></a>
+							
+							<h5>Send Report</h5>
+						</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
 
 	<!-- SECTION: RECENT POSTS & TODOS -->
 	<section id="medic" class="section section-recent">
-
-
-		<div class="col s12 m6 l4">
+	<div class="container">
+  		<div class="jumbotron" style="padding:3px 50px 3px 50px;">
+		  <h4 style="text-align:center;">Prescriptions and TODOs</h4>
+		  </div>
+</div>
+<div class="row">
+		<div class="col s12 m6 l6">
 			<div class="card">
 				<div class="card-content">
-					<span class="card-title">Medicines</span>
+					<span class="card-title">Medicines Suggested By the Doctor</span>
 					<form id="todo-form">
 
 					</form>
 					<ul class="collection todos">
 						<li class="collection-item">
-							<div style="color:green;">Diuretics (twice a day)
+							<div style="color:green;">Diuretics (twice a day) - Dr Nutan Desai
 								<a href="#" class="secondary-content delete">
 									<i class="material-icons">close</i>
 								</a>
 							</div>
 						</li>
 						<li class="collection-item">
-							<div style="color:green;">Beta Blockers (after breakfast)
+							<div style="color:green;">Beta Blockers (after breakfast)-By Dr Nutan Desai
 								<a href="#" class="secondary-content delete">
 									<i class="material-icons">close</i>
 								</a>
 							</div>
 						</li>
 						<li class="collection-item">
-							<div style="color:green;">ACE inhibitors (2 times a day)
+							<div style="color:green;">ACE inhibitors (2 times a day- 
+							By Dr Mahesh Bhatia
 								<a href="#" class="secondary-content delete">
 									<i class="material-icons">close</i>
 								</a>
@@ -257,9 +291,43 @@
 				</div>
 			</div>
 		</div>
+		<div class="col s12 m6 l6">
+			<div class="card">
+				<div class="card-content">
+					<span class="card-title">To DOs</span>
+					<form id="todo-form">
+
+					</form>
+					<ul class="collection todos">
+						<li class="collection-item">
+							<div style="color:green;">Walks (twice a day)
+								<a href="#" class="secondary-content delete">
+									<i class="material-icons">close</i>
+								</a>
+							</div>
+						</li>
+						<li class="collection-item">
+							<div style="color:green;">Yoga 
+								<a href="#" class="secondary-content delete">
+									<i class="material-icons">close</i>
+								</a>
+							</div>
+						</li>
+						
+					</ul>
+				</div>
+			</div>
 		</div>
-	</section>
+	</div>
+
+</section>
+	
 	<section id="map">
+	<div class="container">
+  		<div class="jumbotron" style="padding:3px 50px 3px 50px;">
+		  <h4 style="text-align:center;">Emergency Services</h4>
+		  </div>
+</div>
 	<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1jKnLfqQ69D8z7HEHyOdYR4uFS3ITLw3q" width="100%"
 		height="480"></iframe>
 		</section>
@@ -269,40 +337,6 @@
 	<footer class="section blue darken-2 white-text center">
 		<p>Vriddha Seva Panel Copyright &copy; 2019</p>
 	</footer>
-
-
-
-	<!-- ADD USER MODAL
-      <div id="user-modal" class="modal">
-        <div class="modal-content">
-          <h4>Add User</h4>
-          <form>
-            <div class="input-field">
-              <input type="text" id="name">
-              <label for="name">Name</label>
-            </div>
-            <div class="input-field">
-              <input type="email" id="email">
-              <label for="email">Email</label>
-            </div>
-            <div class="input-field">
-              <input type="password" id="password">
-              <label for="password">Password</label>
-            </div>
-            <div class="input-field">
-              <input type="password" id="confirm-password">
-              <label for="confirm-password">Confirm Password</label>
-            </div>
-          </form>
-
-          
-          <div class="modal-footer">
-            <a href="#" class="modal-action modal-close btn blue white-text">Submit</a>
-          </div>
-        </div>
-      </div> -->
-
-
 
 	<section>
 
